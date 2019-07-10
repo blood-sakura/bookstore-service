1aba4efc90301700474ae9cd3f946796c2e5e4a9
ko79124 <ko79124@LPF0J2DTR.kohlerco.com>
2019-07-10 18:53:20 +0800

    Restrict the recommendListNew() only accessible for HTTP GET requests

--- src/main/java/hello/BookstoreApplication.java
+++ src/main/java/hello/BookstoreApplication.java
@@ -5,19 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 @SpringBootApplication
 @EnableDiscoveryClient
 public class BookstoreApplication {
 
-  @RequestMapping(value = "/recommended")
+  @RequestMapping(value = "/recommended", method = RequestMethod.GET)
   public String readingList(){
     return "Spring in Action (Manning), Cloud Native Java (O'Reilly), Learning Spring Boot (Packt)";
   }
 
 
-  @RequestMapping(value = "/recommendedNew")
+  @RequestMapping(value = "/recommendedNew", method = RequestMethod.GET)
   public String readingListNew(){
     return "New book1, New book2, New book3";
   }
